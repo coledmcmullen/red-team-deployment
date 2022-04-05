5d8ff56e4dc745ea2ac56bfc2efb720f6ffdc612
Zlata Itkin <zitkin@ufl.edu>
2022-04-04 23:56:53 -0400

    some CLI fixes, empty template file for testing

---
+++ template_host.py
@@ -0,0 +1,8 @@
+class TemplateHost(object):
+    """Host from template file."""
+    def __init__(self, file, hostname, description, os):
+        self.file = hostname
+        self.hostname = hostname
+        self.description = description
+        self.os = os
+
--- vcenter-tool.py
+++ vcenter-tool.py
@@ -2,26 +2,70 @@
 
 from cmd import Cmd
 import sys
+import os
+import json
+import template_host
 
 class VCTool(Cmd):
     prompt = 'vctool > '
     intro = "[ VCENTER-TOOL ]\nType help for options.\nType exit or q to quit."
+    hosts = []
+    # I have NO CLUE how vulnerabilities/services are made
+    vulns = []
+    templates = []
 
+    def get_templates_info(self):
+            templates = []
+            f = open('templates/info.json')
+            data = json.load(f)
+            for host in data['templates']:
+                new_temp = template_host.TemplateHost(
+                    host['file'],
+                    host['hostname'],
+                    host['description'],
+                    host['os'])
+                self.templates.append(new_temp)
+            f.close()
+
+    def preloop(self):
+        self.get_templates_info()
     def do_help(self, inp):
-        print("1) add host [name] [OS] [RAM] [processors] [cores] \n2) add vuln [host] [vuln]  \n3) generate script \n4) load script [script] \n5) list hosts \n6) list vuln [host]")
+        print("1) host []\n   host add [host] [name] \n   host list \n   host current \n   host rm [name]")
+        print("3) add vuln [host] [vuln] \n4) generate \n5) load [script] \n6) list vuln [host]")
+
+    def do_host(self, inp):
+        options = inp.split()
+        if len(options) > 0:
+            # vuln or host
+            if options[0] == 'add':
+                if len(options) > 2:
+                    host, name = options[1:3]
+                    self.hosts.append((host, name))
+                else:
+                    print("usage: host add [host] [name]")
+            elif options[0] == 'list':
+                #self.get_templates_info()
+                for t in self.templates:
+                    print(f"\nHost: {t.hostname}\nDesc: {t.description}\nOS: {t.os}")
+                    print("--------------------------------------------------")
+            elif options[0] == 'current':
+                print("[name: host]")
+                for x in self.hosts:
+                    print(f"{x[1]}: {x[0]}")
+            elif options[0] == 'rm':
+                if len(options) > 1:
+                    name = options[1]
+                    self.hosts.remove([host for host in self.hosts if host[1] == name][0])
+                else:
+                    print("usage: host rm [name]")
+            else:
+                print("Type help for options.")
+        else:
+            print("usage: \n   add [host] [name] \n   host list \n   host current \n   host rm [name]")
 
     def do_add(self, inp):
         options = inp.split()
         if len(options) > 0:
-            # vuln or host
-            if options[0] == 'host':
-                if len(options) >= 5:
-                    # Not sure what provisioning will actually be relevant
-                    name, os, ram, procs, cores = options[1:6]
-                    # [Insert relevant actions with this information]
-                else:
-                    print("OS List:")
-                    # Relevant list
             if options[0] == 'vuln':
                 if len(options) > 2:
                     host, vuln = options[1:3]
@@ -30,29 +74,11 @@ class VCTool(Cmd):
                     print("List of vulnerabilities:")
                     # Relevant list                
         else:
-            print("usage: \'add host\' to see OS list\nadd host [name] [OS] [RAM] [processors] [cores]\nadd vuln [host] [vuln]")
+            print("usage: add vuln [host] [vuln]")
 
-    def do_generate(self, inp):
-        if inp == 'script':
-            print("Generate script.")
-            # [Insert relevant actions with this information]
-
-    def do_load(self, inp):
-        options = inp.split()
-        if len(options) < 2:
-            print("usage: load script [script]")
-        else:
-            script = options[1]
-            print("Loading script", script)
-            # [Insert relevant actions with this information]
-    
     def do_list(self, inp):
         options = inp.split()
         if len(options) > 0:
-            # vuln or host
-            if options[0] == 'hosts':
-                print("List of hosts:")
-                # Relevant list
             if options[0] == 'vuln':
                 if len(options) > 1:
                     host = options[1]
@@ -60,7 +86,32 @@ class VCTool(Cmd):
                 else:
                     print("list vuln [host]")              
         else:
-            print("usage: list hosts\nlist vuln [host]")
+            print("usage: list vuln [host]")
+
+    # Does script need name? Assuming yes
+    def do_generate(self, inp):
+        print("Hosts:")
+        if len(self.hosts) > 0:
+            for x in self.hosts:
+                print(f"{x[1]}: {x[0]}")
+        else:
+            print("No hosts.")
+        print("Services:") # Vulnerabilities??
+        if_gen = input("Would you like to generate a script with this information? (y/n): ")
+        if if_gen.lower() == 'n' or if_gen.lower() != 'y':
+            print("Not generating a script.")
+        else:
+            scr_name = input("Script name: ")
+            print(f"Generating script {scr_name}...")
+
+    def do_load(self, inp):
+        options = inp.split()
+        if len(options) < 1:
+            print("usage: load [script]")
+        else:
+            script = options[0]
+            print("Loading script", script)
+            # [Insert relevant actions with this information]    
     
     def do_exit(self, inp):
         print("Bye.")
@@ -71,7 +122,9 @@ class VCTool(Cmd):
             return self.do_exit(inp)
 
     do_EOF = do_exit
-   
+
+
+
 if len(sys.argv) > 1:
     VCTool().onecmd(' '.join(sys.argv[1:]))
 else:
