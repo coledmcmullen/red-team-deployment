6ed010435601d1588f1ab04399b0dd69a614ab9d
Zlata <zitkin@outlook.com>
2022-04-05 14:46:20 -0400

    host files created upon generating script

--- TemplateHost.py
+++ TemplateHost.py
@@ -1,7 +1,7 @@
 class TemplateHost(object):
     """Host from template file."""
     def __init__(self, file, hostname, description, os):
-        self.file = hostname
+        self.file = file
         self.hostname = hostname
         self.description = description
         self.os = os
--- vcenter-tool.py
+++ vcenter-tool.py
@@ -5,6 +5,7 @@ import sys
 import os
 import json
 import TemplateHost
+from TemplateHost import TemplateHost
 
 class VCTool(Cmd):
     prompt = 'vctool > '
@@ -14,19 +15,21 @@ class VCTool(Cmd):
     vulns = []
     templates = []
 
+    ## Pull relevant host information from JSON file
     def get_templates_info(self):
             templates = []
-            f = open('templates/info.json')
-            data = json.load(f)
-            for host in data['templates']:
-                new_temp = template_host.TemplateHost(
-                    host['file'],
-                    host['hostname'],
-                    host['description'],
-                    host['os'])
-                templates.append(new_temp)
-            f.close()
+            with open('templates/info.json') as f:
+                data = json.load(f)
+                for host in data['templates']:
+                    new_temp = TemplateHost(
+                        host['file'],
+                        host['hostname'],
+                        host['description'],
+                        host['os'])
+                    templates.append(new_temp)
             self.templates = templates
+    
+    ## Get host information only once
     def preloop(self):
         self.get_templates_info()
 
@@ -34,33 +37,56 @@ class VCTool(Cmd):
         self.get_templates_info()
         return line
 
+    ## List of commands
     def do_help(self, inp):
         print("1) host []\n   host add [host] [name] \n   host list \n   host current \n   host rm [name]")
         print("3) add vuln [host] [vuln] \n4) generate \n5) load [script] \n6) list vuln [host]")
 
+    ## All host related actions: add host, list hosts available, list current hosts, and remove host
     def do_host(self, inp):
         options = inp.split()
         if len(options) > 0:
             # vuln or host
             if options[0] == 'add':
                 if len(options) > 2:
-                    host, name = options[1:3]
-                    self.hosts.append((host, name))
+                    hostname, name = options[1:3]
+
+                    host = None
+                    for temp in self.templates:
+                        if temp.hostname == hostname:
+                            host = temp
+                            break
+
+                    if host is not None:
+                        self.hosts.append((host, name))
+                    else:
+                        print("Incorrect host.")
                 else:
                     print("usage: host add [host] [name]")
+
             elif options[0] == 'list':
-                #self.get_templates_info()
                 for t in self.templates:
                     print(f"\nHost: {t.hostname}\nDesc: {t.description}\nOS: {t.os}")
                     print("--------------------------------------------------")
+
             elif options[0] == 'current':
                 print("[name: host]")
                 for x in self.hosts:
-                    print(f"{x[1]}: {x[0]}")
+                    print(f"{x[1]}: {x[0].hostname}")
+
             elif options[0] == 'rm':
                 if len(options) > 1:
                     name = options[1]
-                    self.hosts.remove([host for host in self.hosts if host[1] == name][0])
+                    rm_host = None
+                    if len(self.hosts) > 0:
+                        for host in self.hosts:
+                            if host[1] == name:
+                                rm_host = host
+                                break
+                        if rm_host is not None:
+                            self.hosts.remove(rm_host)
+                    else:
+                        print("No hosts to remove.")
                 else:
                     print("usage: host rm [name]")
             else:
@@ -93,12 +119,12 @@ class VCTool(Cmd):
         else:
             print("usage: list vuln [host]")
 
-    # Does script need name? Assuming yes
+    ## Generate laforge script
     def do_generate(self, inp):
         print("Hosts:")
         if len(self.hosts) > 0:
             for x in self.hosts:
-                print(f"{x[1]}: {x[0]}")
+                print(f"{x[1]}: {x[0].hostname}")
         else:
             print("No hosts.")
         print("Services:") # Vulnerabilities??
@@ -108,8 +134,10 @@ class VCTool(Cmd):
         else:
             scr_name = input("Script name: ")
             print(f"Generating script {scr_name}...")
-            # Here, make host files
 
+            self.create_hosts()
+
+    ## Load laforge script (and deploy?)
     def do_load(self, inp):
         options = inp.split()
         if len(options) < 1:
@@ -129,7 +157,15 @@ class VCTool(Cmd):
 
     do_EOF = do_exit
 
-
+    ## Create necessary host files from user information
+    def create_hosts(self):
+        for h in self.hosts:
+            template_file = h[0].file
+            with open(f'templates/{template_file}', 'r') as tf:
+                data = tf.readlines()
+            data[2] = f'  hostname = \"{h[1]}\"\n'
+            with open(f'createdhosts/{h[1]}.laforge.tmpl', 'w') as f:
+                f.writelines(data)
 
 if len(sys.argv) > 1:
     VCTool().onecmd(' '.join(sys.argv[1:]))
