d337ce3e80237d07a7b69cc1a7871986ac6a7e54
NickTateo <tateo.nick@gmail.com>
2022-04-09 03:03:31 -0400

    more API functionality

--- api_calls.py
+++ api_calls.py
@@ -2,6 +2,7 @@
 
 import requests
 import json
+#import urllib
 #import logging
 
 #logging.captureWarnings(True)
@@ -23,33 +24,70 @@ def printError(err):
 
 def getSessionToken(api_base, api_creds):
 	sess = requests.post("{}/api/session".format(api_base), auth=(api_creds["user"], api_creds["password"]), verify=True)
-	session_id = sess.json()
+	session_tok = sess.json()
 
 	if(sess.status_code == 201):
-		return session_id;
+		return session_tok;
 	else:
 		#TODO
 		printError("Failed to get session token")
 		return -1
 
-def getVMList(api_base, session_id):
+def getVMList(api_base, session_tok, vm_names):
 	resp = requests.get("{}/api/vcenter/vm".format(api_base), verify=True, headers={
-	    "vmware-api-session-id": session_id
+		"vmware-api-session-id": session_tok
+	},
+	params = {
+		#"folders":[urllib.parse.quote_plus("Spring 2022 Laforge Senior Project")]
+		"names": vm_names
+		#"power_states":["POWERED_ON"]
+		#"vms":["vm-18106"]
 	})
-
+	print(resp.status_code)
 	return resp.json()
+	#return resp.json()[0]["vm"]
 
 #def createVM():
 	#TODO
 
-#def cloneVM():
-	#TODO
+def cloneVM(api_base, session_tok, source_id, clone_name):
+	resp = requests.post("{}/api/vcenter/vm?action=clone".format(api_base), verify=True, headers={
+		"vmware-api-session-id": session_tok
+	},
+	json = {
+		"name": clone_name,
+		"source": source_id
+	})
+	#print(resp.status_code)
+	return resp.json()
 
-#def deleteVM():
-	#TODO
+#204 success, 400 VM powered on, 404 not found
+def deleteVM(api_base, session_tok, vm_id):
+	resp = requests.delete("{}/api/vcenter/vm/{}".format(api_base, vm_id), verify=True, headers={
+		"vmware-api-session-id": session_tok
+	})
+	return resp.json()
 
-#def getVMInfo():
-	#TODO
+
+def getVMInfo(api_base, session_tok, vm_id):
+	resp = requests.get("{}/api/vcenter/vm/{}".format(api_base, vm_id), verify=True, headers={
+		"vmware-api-session-id": session_tok
+	})
+	return resp.json()
+
+#204 sucess, 400 VM already in desired state, 404 VM not found
+def powerOnVM(api_base, session_tok, vm_id):
+	resp = requests.post("{}/api/vcenter/vm/{}/power?action=start".format(api_base, vm_id), verify=True, headers={
+		"vmware-api-session-id": session_tok
+	})
+	return resp.status_code
+
+#204 sucess, 400 VM already in desired state, 404 VM not found
+def powerOffVM(api_base, session_tok, vm_id):
+	resp = requests.post("{}/api/vcenter/vm/{}/power?action=stop".format(api_base, vm_id), verify=True, headers={
+		"vmware-api-session-id": session_tok
+	})
+	return resp.status_code
 
 #def startVMProcess():
 	#TODO
--- backend.py
+++ backend.py
@@ -12,5 +12,11 @@ def init():
 
 def testCalls():
 	conf = init()
-	print(getVMList(conf["api_base"], conf["session_token"]))
+	test_id = getVMList(conf["api_base"], conf["session_token"], ["test2_kali"])[0]["vm"]
+	print(test_id)
+	#print(getVMInfo(conf["api_base"], conf["session_token"], test_id))
+	#print(powerOffVM(conf["api_base"], conf["session_token"], test_id))
+	#print(powerOnVM(conf["api_base"], conf["session_token"], test_id))
+	#print(cloneVM(conf["api_base"], conf["session_token"], test_id, "test2_kali"))
+	print(deleteVM(conf["api_base"], conf["session_token"], test_id))
 
