6980f97efbe79bb455d2af74dc4104f0269e0552
NickTateo <tateo.nick@gmail.com>
2022-04-10 15:46:25 -0400

    Host json examples

--- api_calls.py
+++ api_calls.py
@@ -2,36 +2,27 @@
 
 import requests
 import json
-#import urllib
 #import logging
 
 #logging.captureWarnings(True)
 
-def loadConf(conf_filepath):
-	#conf =
-	try:
-		f = open(conf_filepath)
-		conf = json.load(f)
-		f.close()
-	except Exception as e:
-		#TODO
-		printError(e)
-		return -1
-	return conf
 
 def printError(err):
 	print("Error: %s" % err)
 
+
 def getSessionToken(api_base, api_creds):
 	sess = requests.post("{}/api/session".format(api_base), auth = (api_creds["user"], api_creds["password"]), verify = True)
-	session_tok = sess.json()
-
+	
 	if(sess.status_code == 201):
-		return session_tok;
+		print("Retrieved new session token")
 	else:
 		#TODO
 		printError("Failed to get session token")
-		return -1
+		#return -1
+	
+	return sess.json()
+
 
 def getVMIDs(api_base, session_tok, vm_names):
 	resp = requests.get("{}/api/vcenter/vm".format(api_base), verify = True, headers = {
@@ -41,11 +32,10 @@ def getVMIDs(api_base, session_tok, vm_names):
 		"names": vm_names
 	})
 
-	#Check status code and throw err if not successful
+	#Remove - Check status code and throw err if not successful
 	print(resp.status_code)
 	#return just ID or entire json to caller? Restrict to singular ID or allow multiple ID retrieval?
 	return resp.json()
-	#return resp.json()[0]["vm"]
 
 
 def listVMFolder(api_base, session_tok, vm_folders):
@@ -58,12 +48,14 @@ def listVMFolder(api_base, session_tok, vm_folders):
 		#"power_states": ["POWERED_ON"]
 		#"vms": ["vm-18106"]
 	})
+	#Remove
 	print(resp.status_code)
 	return resp.json()
-	#return resp.json()[0]["vm"]
+
 
 #def createVM():
-	#TODO
+	#TODO?
+
 
 def cloneVM(api_base, session_tok, source_id, clone_name):
 	resp = requests.post("{}/api/vcenter/vm?action=clone".format(api_base), verify = True, headers = {
@@ -75,6 +67,7 @@ def cloneVM(api_base, session_tok, source_id, clone_name):
 	})
 	#print(resp.status_code)
 	return resp.json()
+
 
 #204 success, 400 VM powered on, 404 not found
 def deleteVM(api_base, session_tok, vm_id):
@@ -90,27 +83,43 @@ def getVMInfo(api_base, session_tok, vm_id):
 	})
 	return resp.json()
 
+
 #204 sucess, 400 VM already in desired state, 404 VM not found
 def powerOnVM(api_base, session_tok, vm_id):
 	resp = requests.post("{}/api/vcenter/vm/{}/power?action=start".format(api_base, vm_id), verify = True, headers = {
 		"vmware-api-session-id": session_tok
 	})
-	return resp.status_code
+	return resp.json()
+
 
 #204 sucess, 400 VM already in desired state, 404 VM not found
 def powerOffVM(api_base, session_tok, vm_id):
 	resp = requests.post("{}/api/vcenter/vm/{}/power?action=stop".format(api_base, vm_id), verify = True, headers = {
 		"vmware-api-session-id": session_tok
 	})
-	return resp.status_code
+	return resp.json()
 
-def startVMProcess(api_base, session_tok, vm_id, guest_creds, proc_path):
+
+def startVMProcess(api_base, session_tok, vm_id, guest_login, proc_spec):
 	resp = requests.post("{}/api/vcenter/vm/{}/guest/processes?action=create".format(api_base, vm_id), verify = True, headers = {
 		"vmware-api-session-id": session_tok
 	},
 	json = {
-		"credentials": guest_creds,
-		"spec": proc_path
+		"credentials": guest_login,
+		"spec": proc_spec
 	})
+	#Remove
+	print(resp.status_code)
+	return resp.json()
+	
+	
+def killVMProcess(api_base, session_tok, vm_id, guest_login, pid):
+	resp = requests.post("{}/api/vcenter/vm/{}/guest/processes/{}?action=delete".format(api_base, vm_id, pid), verify = True, headers = {
+		"vmware-api-session-id": session_tok
+	},
+	json = {
+		"credentials": guest_login
+	})
+	#Remove
 	print(resp.status_code)
 	return resp.json()
--- backend.py
+++ backend.py
@@ -3,40 +3,102 @@
 from api_calls import *
 
 #API configuration file relative path
-conf_path = "./api_conf.json"
-guest_creds = {
+conf_path = "./api_conf"
+
+#TODO isolate into file
+#guest_conf_path = ""
+guest_login = {
 	"interactive_session": True,
-	"password": "ufcptc2020!",
+	"password": "<GUEST_PASS>",
 	"type": "USERNAME_PASSWORD",
-	"user_name": "ufcptc"
+	"user_name": "<GUEST_USER>"
 }
+#TODO support optional spec args
 proc_spec = {
-	#"path": "/usr/bin/top"
 	"path": "/usr/lib/firefox-esr/firefox-esr"
 }
 
+def loadConf(fpath):
+	#conf =
+	try:
+		f = open(fpath)
+		conf = json.load(f)
+		f.close()
+	except Exception as e:
+		#TODO
+		printError(e)
+		return -1
+	return conf
+
+def writeConf(fpath, data):
+	try:
+		f = open(fpath, "w")
+		f.write(data)
+		f.close()
+	except Exception as e:
+		#TODO
+		printError(e)
+		return -1
+	return 0
+
 def init():
 	conf = loadConf(conf_path)
-	conf["session_token"] = getSessionToken(conf["api_base"], conf["creds"])
+	#if(conf["session_token"] == "" or conf["session_token"] == -1):
+	#conf["session_token"] = getSessionToken(conf["api_base"], conf["creds"])
+	#writeConf(conf_path, json.dumps(conf, indent = 4))
 	return conf
+
+def testHostsModel():
+	#hosts = []
+	hosts = [loadConf("target2.json")]
+	print(hosts[0], hosts)
+	hosts[0]["id"] = 2
+	print(hosts[0], hosts)
+	hosts[0]["name"] = "target"
+	writeConf("target.json", json.dumps(hosts[0], indent = 4))
 
 def testCalls():
 	conf = init()
-	test_resp = getVMIDs(conf["api_base"], conf["session_token"], ["test_kali"])
+	#test_resp = getVMIDs(conf["api_base"], conf["session_token"], ["test_target"])
 	test_id = test_resp[0]["vm"]
 	#test_resp = listVMFolder(conf["api_base"], conf["session_token"], ["Spring_2022_Laforge_Senior_Project"])
-	print(test_resp)
-	print(test_id)
+	#print(test_resp)
+	#print(test_id)
 	#print(getVMInfo(conf["api_base"], conf["session_token"], test_id))
 	#print(getVMInfo(conf["api_base"], conf["session_token"], ["rtdt_live"]))
 	#print(powerOffVM(conf["api_base"], conf["session_token"], test_id))
 	#print(powerOnVM(conf["api_base"], conf["session_token"], test_id))
-	#print(cloneVM(conf["api_base"], conf["session_token"], test_id, "test2_kali"))
+	#print(cloneVM(conf["api_base"], conf["session_token"], test_id, "test_target"))
 	#print(deleteVM(conf["api_base"], conf["session_token"], test_id))
-	test_resp = startVMProcess(conf["api_base"], conf["session_token"], test_id, guest_creds, proc_spec)
-	print(test_resp)
+	#test_resp = startVMProcess(conf["api_base"], conf["session_token"], test_id, guest_login, proc_spec)
+	#print(test_resp)
 
-#def addHost():
+def addHost(source_name, clone_name):
+	resp = getVMIDs(conf["api_base"], conf["session_token"], source_name)
+	#TODO - check getID success
+		source_id = resp[0]["vm"]
+		resp = cloneVM(conf["api_base"], conf["session_token"], source_id, clone_name)
+
+		if(resp.status_code == 200):
+			print("Adding host %s..." % clone_name)
+			#write new host json
+		else:
+			print("Failed to add host %s" % clone_name)
+
+#def removeHost(conf, host_name):
+	resp = getVMIDs(conf["api_base"], conf["session_token"], host_name)
+	host_id = resp[0]["vm"]
+	#TODO - check getID success
+		resp = deleteVM(conf["api_base"], conf["session_token"], host_id)
+		if resp.status_code == 204:
+			#remove host from hosts array
+			#delete json file
+		else:
+			#print err
 
 
-#def addVuln():
+#def addVuln(conf, host_name, service):
+	resp = getVMIDs(conf["api_base"], conf["session_token"], host_name)
+	host_id = resp[0]["vm"]
+	#TODO - check getID success
+		resp = startVMProcess(conf["api_base"], conf["session_token"], host_id, guest_login)
