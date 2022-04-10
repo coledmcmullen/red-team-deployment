1f53be7172ac1a70d4bf2ff63dc4746a421e2f38
NickTateo <tateo.nick@gmail.com>
2022-04-09 21:40:13 -0400

    process control

--- api_calls.py
+++ api_calls.py
@@ -23,7 +23,7 @@ def printError(err):
 	print("Error: %s" % err)
 
 def getSessionToken(api_base, api_creds):
-	sess = requests.post("{}/api/session".format(api_base), auth=(api_creds["user"], api_creds["password"]), verify=True)
+	sess = requests.post("{}/api/session".format(api_base), auth = (api_creds["user"], api_creds["password"]), verify = True)
 	session_tok = sess.json()
 
 	if(sess.status_code == 201):
@@ -33,15 +33,30 @@ def getSessionToken(api_base, api_creds):
 		printError("Failed to get session token")
 		return -1
 
-def getVMList(api_base, session_tok, vm_names):
-	resp = requests.get("{}/api/vcenter/vm".format(api_base), verify=True, headers={
+def getVMIDs(api_base, session_tok, vm_names):
+	resp = requests.get("{}/api/vcenter/vm".format(api_base), verify = True, headers = {
 		"vmware-api-session-id": session_tok
 	},
 	params = {
-		#"folders":[urllib.parse.quote_plus("Spring 2022 Laforge Senior Project")]
 		"names": vm_names
-		#"power_states":["POWERED_ON"]
-		#"vms":["vm-18106"]
+	})
+
+	#Check status code and throw err if not successful
+	print(resp.status_code)
+	#return just ID or entire json to caller? Restrict to singular ID or allow multiple ID retrieval?
+	return resp.json()
+	#return resp.json()[0]["vm"]
+
+
+def listVMFolder(api_base, session_tok, vm_folders):
+	resp = requests.get("{}/api/vcenter/vm".format(api_base), verify = True, headers = {
+		"vmware-api-session-id": session_tok
+	},
+	params = {
+		#"folders": [urllib.parse.quote_plus("Spring 2022 Laforge Senior Project")]
+		"folders": vm_folders
+		#"power_states": ["POWERED_ON"]
+		#"vms": ["vm-18106"]
 	})
 	print(resp.status_code)
 	return resp.json()
@@ -51,7 +66,7 @@ def getVMList(api_base, session_tok, vm_names):
 	#TODO
 
 def cloneVM(api_base, session_tok, source_id, clone_name):
-	resp = requests.post("{}/api/vcenter/vm?action=clone".format(api_base), verify=True, headers={
+	resp = requests.post("{}/api/vcenter/vm?action=clone".format(api_base), verify = True, headers = {
 		"vmware-api-session-id": session_tok
 	},
 	json = {
@@ -63,31 +78,39 @@ def cloneVM(api_base, session_tok, source_id, clone_name):
 
 #204 success, 400 VM powered on, 404 not found
 def deleteVM(api_base, session_tok, vm_id):
-	resp = requests.delete("{}/api/vcenter/vm/{}".format(api_base, vm_id), verify=True, headers={
+	resp = requests.delete("{}/api/vcenter/vm/{}".format(api_base, vm_id), verify = True, headers = {
 		"vmware-api-session-id": session_tok
 	})
 	return resp.json()
 
 
 def getVMInfo(api_base, session_tok, vm_id):
-	resp = requests.get("{}/api/vcenter/vm/{}".format(api_base, vm_id), verify=True, headers={
+	resp = requests.get("{}/api/vcenter/vm/{}".format(api_base, vm_id), verify = True, headers = {
 		"vmware-api-session-id": session_tok
 	})
 	return resp.json()
 
 #204 sucess, 400 VM already in desired state, 404 VM not found
 def powerOnVM(api_base, session_tok, vm_id):
-	resp = requests.post("{}/api/vcenter/vm/{}/power?action=start".format(api_base, vm_id), verify=True, headers={
+	resp = requests.post("{}/api/vcenter/vm/{}/power?action=start".format(api_base, vm_id), verify = True, headers = {
 		"vmware-api-session-id": session_tok
 	})
 	return resp.status_code
 
 #204 sucess, 400 VM already in desired state, 404 VM not found
 def powerOffVM(api_base, session_tok, vm_id):
-	resp = requests.post("{}/api/vcenter/vm/{}/power?action=stop".format(api_base, vm_id), verify=True, headers={
+	resp = requests.post("{}/api/vcenter/vm/{}/power?action=stop".format(api_base, vm_id), verify = True, headers = {
 		"vmware-api-session-id": session_tok
 	})
 	return resp.status_code
 
-#def startVMProcess():
-	#TODO
+def startVMProcess(api_base, session_tok, vm_id, guest_creds, proc_path):
+	resp = requests.post("{}/api/vcenter/vm/{}/guest/processes?action=create".format(api_base, vm_id), verify = True, headers = {
+		"vmware-api-session-id": session_tok
+	},
+	json = {
+		"credentials": guest_creds,
+		"spec": proc_path
+	})
+	print(resp.status_code)
+	return resp.json()
--- backend.py
+++ backend.py
@@ -4,6 +4,16 @@ from api_calls import *
 
 #API configuration file relative path
 conf_path = "./api_conf.json"
+guest_creds = {
+	"interactive_session": True,
+	"password": "ufcptc2020!",
+	"type": "USERNAME_PASSWORD",
+	"user_name": "ufcptc"
+}
+proc_spec = {
+	#"path": "/usr/bin/top"
+	"path": "/usr/lib/firefox-esr/firefox-esr"
+}
 
 def init():
 	conf = loadConf(conf_path)
@@ -12,11 +22,21 @@ def init():
 
 def testCalls():
 	conf = init()
-	test_id = getVMList(conf["api_base"], conf["session_token"], ["test2_kali"])[0]["vm"]
+	test_resp = getVMIDs(conf["api_base"], conf["session_token"], ["test_kali"])
+	test_id = test_resp[0]["vm"]
+	#test_resp = listVMFolder(conf["api_base"], conf["session_token"], ["Spring_2022_Laforge_Senior_Project"])
+	print(test_resp)
 	print(test_id)
 	#print(getVMInfo(conf["api_base"], conf["session_token"], test_id))
+	#print(getVMInfo(conf["api_base"], conf["session_token"], ["rtdt_live"]))
 	#print(powerOffVM(conf["api_base"], conf["session_token"], test_id))
 	#print(powerOnVM(conf["api_base"], conf["session_token"], test_id))
 	#print(cloneVM(conf["api_base"], conf["session_token"], test_id, "test2_kali"))
-	print(deleteVM(conf["api_base"], conf["session_token"], test_id))
+	#print(deleteVM(conf["api_base"], conf["session_token"], test_id))
+	test_resp = startVMProcess(conf["api_base"], conf["session_token"], test_id, guest_creds, proc_spec)
+	print(test_resp)
 
+#def addHost():
+
+
+#def addVuln():
